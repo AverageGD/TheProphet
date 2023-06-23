2af18d6d8196a6956c5ca26a7cc77f0457a2b019
AverageGD <edgargames333@gmail.com>
2023-06-23 18:25:48 +0400

    0.0.1
    Added Attack's basic logic, added wallclimb etc

--- The Prophet/Assets/Scripts/Player/CharacterController2D.cs
+++ The Prophet/Assets/Scripts/Player/CharacterController2D.cs
@@ -1,50 +1,84 @@
 using System.Collections;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
-using static UnityEngine.ParticleSystem;
+
 
 public class CharacterController2D : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private LayerMask _enemyLayer;
     [SerializeField] private float _acceleration;
     [SerializeField] private float _decceleration;
     [SerializeField] private float _velocityPower;
-    [SerializeField] private Transform _groundCheckerTransform;
+    [SerializeField] private Transform _groundChecker;
+    [SerializeField] private Transform _wallChecker;
     [SerializeField] private Collider2D _playerCollider;
+    [SerializeField] private Transform _attackPoint;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
 
     private float horizontalAxis;
     private Rigidbody2D rigidBody;
     private bool canDash = true;
+    private bool canAttack = true;
     private bool isDashing;
+    private bool isAttacking;
     private short direction;
+    private short currentNumberOfAttacks = 0;
+    private short damage = 1;
+    private float lastJumpTime;
 
     public float dashPower;
+    public short upgradeLevel;
+
 
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+
+        direction = 1;
+        _spriteRenderer.flipX = false;
     }
 
     private void Update()
     {
-        if (isDashing) return;
+        if (IsWallNear())
+        {
+            print("wall is near!");
+            _playerCollider.enabled = true;
+        }
+
+        if (isDashing || isAttacking) return;
 
         horizontalAxis = Input.GetAxisRaw("Horizontal");
 
-        direction = (short)(horizontalAxis >= 0 ? 1 : -1);
+        if (horizontalAxis > 0)
+
+            direction = 1;
+
+        else if (horizontalAxis < 0)
+
+            direction = -1;
+
+
+        _attackPoint.localPosition = new Vector2(direction * 0.63f, _attackPoint.localPosition.y);
+
+        _wallChecker.localPosition = new Vector2(direction * 0.36f, _wallChecker.localPosition.y);
     }
 
     public void Jump()
     {
-        if (IsGrounded())
+        if (IsGrounded() || upgradeLevel >= 2 && IsWallNear() && Time.time - lastJumpTime > 0.3f)
         {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 18f);
+            lastJumpTime = Time.time;
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, 8f);
         }
+    }
 
-        if (rigidBody.velocity.y > 0f)
+    public void AttackInvoker()
+    {
+        if (canAttack)
         {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y * 0.5f);
+            StartCoroutine(Attack());
         }
     }
 
@@ -63,6 +97,11 @@ public class CharacterController2D : MonoBehaviour
         isDashing = true;
         float origGravity = rigidBody.gravityScale; // keeps default gravity scale
 
+        if (upgradeLevel >= 5)
+        {
+            //strikes damage to enemies
+        }
+
         rigidBody.gravityScale = 0;
         rigidBody.velocity = Vector2.zero;
         rigidBody.velocity = new Vector2(direction * dashPower, 0f); // adds speed with player's current direction
@@ -79,6 +118,38 @@ public class CharacterController2D : MonoBehaviour
         canDash = true;
     }
 
+    private IEnumerator Attack()
+    {
+        canAttack = false;
+        isAttacking = true;
+
+        print("lock");
+
+        short maxNumberOfAttacks = (short)(upgradeLevel >= 1 ? 4 : 3);
+        currentNumberOfAttacks++;
+
+        if (currentNumberOfAttacks > maxNumberOfAttacks)
+        {
+            currentNumberOfAttacks = 1;
+        }
+
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(_attackPoint.position, 0.2f, _enemyLayer);
+
+        foreach (Collider2D enemy in enemies)
+        {
+            if (enemy != null)
+            {
+                // When enemies are created here will be calling function for taking damage
+            }
+        }
+
+        yield return new WaitForSeconds(0.3f);
+
+        print("open");
+
+        canAttack = true;
+        isAttacking = false;
+    }
     private void FixedUpdate()
     {
         //basic movement logic
@@ -97,12 +168,19 @@ public class CharacterController2D : MonoBehaviour
     //Checks if player is grounded
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(_groundCheckerTransform.position, 0.3f, _groundLayer);
+        return Physics2D.OverlapCircle(_groundChecker.position, 0.3f, _groundLayer);
     }
 
+    private bool IsWallNear()
+    {
+        return Physics2D.OverlapCircle(_wallChecker.position, 0.2f, _groundLayer);
+    }
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.DrawWireSphere(_groundCheckerTransform.position, 0.3f);
+        Gizmos.DrawWireSphere(_groundChecker.position, 0.3f);
+        Gizmos.DrawWireSphere(_wallChecker.position, 0.2f);
+        Gizmos.DrawWireSphere(_attackPoint.position, 0.2f);
     }
+
 }
