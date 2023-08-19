611e8155ea0da5e770b145ab2765fa2213782571
AverageGD <edgargames333@gmail.com>
2023-08-20 00:04:55 +0400

    0.0.5
    Created minimap system(need to determine positions of sprites)

--- The Prophet/Assets/Scripts/RoomsUIController.cs
+++ The Prophet/Assets/Scripts/RoomsUIController.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomsUIController : MonoBehaviour
 {
+    [SerializeField] private GameObject roomUI;
+    [SerializeField] private Transform map;
+
     private bool isMapOpen;
     public void TryToTurnOnOffMap()
     {
@@ -16,6 +21,22 @@ public class RoomsUIController : MonoBehaviour
 
     private void ListRooms()
     {
+        foreach (Transform room in map)
+        {
+            Destroy(room.gameObject);
+        }
 
+        foreach (KeyValuePair<int, bool> key in AllRoomsContainer.instance.visitedRoomsDictionary)
+        {
+            if (key.Value)
+            {
+                GameObject roomUIClone = Instantiate(roomUI, transform);
+
+                roomUIClone.transform.localPosition = AllRoomsContainer.instance.roomsDictionary[key.Key].GetComponent<RoomInfo>().roomSpriteCoordinates;
+                roomUIClone.GetComponent<Image>().sprite = AllRoomsContainer.instance.roomsDictionary[key.Key].GetComponent<RoomInfo>().roomSprite;
+                roomUIClone.GetComponent<Image>().SetNativeSize();
+            }
+
+        }
     }
 }
