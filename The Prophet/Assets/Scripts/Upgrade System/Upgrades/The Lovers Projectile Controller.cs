14e3d745ef41a80adc843fb55f22ee20df4ed3c6
AverageGD <edgargames333@gmail.com>
2023-09-01 12:18:12 +0400

    The Lovers skill
    Added The Lover skill's logic(autotarget projectile)

---
+++ The Prophet/Assets/Scripts/Upgrade System/Upgrades/The Lovers Projectile Controller.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class TheLoversProjectileController : MonoBehaviour
+{
+    public float radius = 4f;
+    public float smoothTimeIdle = 0.25f;
+    public float smoothTimeAttack = 0.05f;
+    public float damage = 1f;
+    public LayerMask enemyLayer;
+
+    private Transform target;
+    private Vector2 currentVelocity = Vector2.zero;
+
+    private void Update()
+    {
+        float smoothTime;
+
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(transform.position, radius, enemyLayer);
+
+        if (enemies.Length > 0)
+        {
+            target = enemies[0].transform;
+            smoothTime = smoothTimeAttack;
+        } else
+        {
+            target = CharacterController2D.instance.transform;
+            smoothTime = smoothTimeIdle;
+        }
+
+        if (target != null)
+            transform.position = Vector2.SmoothDamp(transform.position, target.position, ref currentVelocity, smoothTime);
+
+        Vector2 directionToTarget = (target.position - transform.position).normalized;
+
+        float angle = Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<EnemyHealthController>().TakeDamage(damage);
+
+            Destroy(gameObject);
+        }
+    }
+}
--- The Prophet/Assets/Scripts/Upgrade System/Upgrades/TheLoversUpgrade.cs
+++ The Prophet/Assets/Scripts/Upgrade System/Upgrades/TheLoversUpgrade.cs
@@ -1,12 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [CreateAssetMenu]
 public class TheLoversUpgrade : UpgradeAbility
 {
+    [SerializeField] private float _radius = 4f;
+    [SerializeField] private float _smoothTimeIdle = 0.25f;
+    [SerializeField] private float _smoothTimeAttack = 0.05f;
+    [SerializeField] private float _damage = 1f;
+    [SerializeField] private LayerMask _enemyLayer;
+    [SerializeField] private GameObject _projectile;
+    [SerializeField] private int _projectileMaxCount = 4;
+
+    private List<GameObject> projectiles;
+
     public override void Activate()
     {
         base.Activate();
 
-        Debug.Log("The Lovers are purchased and now are working");
+        projectiles = new List<GameObject>();
+
+        UpgradeSystemManager.instance.StartCoroutine(ProjectileSpawn());
     }  
+
+    private IEnumerator ProjectileSpawn()
+    {
+        float elapsedTime = 0f;
+
+        while (true)
+        {
+            elapsedTime += Time.deltaTime;
+
+            for (int i = 0; i < projectiles.Count; i++)
+            {
+                if (projectiles[i] == null)
+                {
+                    projectiles.RemoveAt(i);
+                    elapsedTime = 0f;
+                }
+            }
+
+            if (projectiles.Count < _projectileMaxCount && elapsedTime > 4f)
+            {
+                elapsedTime = 0;
+
+                GameObject projectileClone = Instantiate(_projectile);
+                Debug.Log("Spawn Projectile");
+                TheLoversProjectileController theLoversProjectileController = projectileClone.GetComponent<TheLoversProjectileController>();
+
+                projectileClone.transform.position = CharacterController2D.instance.transform.position;
+
+                theLoversProjectileController.radius = _radius;
+                theLoversProjectileController.smoothTimeAttack = _smoothTimeAttack;
+                theLoversProjectileController.smoothTimeIdle = _smoothTimeIdle;
+                theLoversProjectileController.damage = _damage;
+                theLoversProjectileController.enemyLayer = _enemyLayer;
+
+                projectiles.Add(projectileClone);
+            }
+
+            yield return null;
+        }
+    }
 }
